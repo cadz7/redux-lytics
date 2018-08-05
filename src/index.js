55979200d9fe24c1a888cfb2310df30f7de9feee
Rick Shukla <rakshit.shukla@tophat.com>
2018-08-04 22:53:57 -0400

    Updated readme

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function createLogger(eventMap = {}) {
+    return store => next => action => {
+        if (action.type in eventMap) {
+            if (navigator.sendBeacon) {
+                let result = navigator.sendBeacon(eventMap[action.type].url, JSON.stringify(eventMap[action.type].data));
+                if (!result) console.log('Failure.');
+            } else {
+                return fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(eventMap[action.type].data)
+                })
+            }
+        }
+        return next(action)
+    }
+}
+    
+export { createLogger }
