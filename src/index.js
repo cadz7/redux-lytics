fe7dd12cc89400d4b6404c194be2c9b0afdfe1ef
Rakshit Shukla <shuklapossible@gmail.com>
2018-10-21 00:31:56 -0400

    feat: basic incremental retry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import 'whatwg-fetch'
 
+const rQueue = []
+let totalCalls = 0
+let lastTime = new Date()
+
 function _printUnexpectedActionPayload() {
     console.error( // eslint-disable-line no-console
         `Unexpected config provided. Please check https://github.com/cadz7/redux-lytics for reference.`
     );
 }
+
+function fireRequest(payload) {
+    totalCalls += 1
+    fetch(payload.url, {
+        method: "POST",
+        body: JSON.stringify(payload.body),
+        headers: payload.headers
+    })
+    .then(resp => {
+        if (resp.status > 400) {
+            rQueue.push({ url: payload.url, body: payload.body, headers: payload.headers})
+        } else {
+            totalCalls -= 1
+        }
+    })
+}
+
+function fireIfAvailable() {
+    if (rQueue.length > 0) {
+        const currTime = new Date()
+        if (currTime - lastTime < 2000) {
+            return
+        }
+        const r = rQueue.pop()
+        fireRequest({ url: r.url, body: r.body, headers: r.headers })
+        lastTime = currTime
+        fireIfAvailable()
+    }
+}
+
+setInterval(fireIfAvailable, 1000)
 
 function createLogger(actionPayload = {}, defaultHeaders = {}) {
     return store => next => action => {
@@ -32,11 +67,9 @@ function createLogger(actionPayload = {}, defaultHeaders = {}) {
                 return next(action)
             }
             
-            fetch(url, {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: headers
-            }).catch(() => {})
+            const payload = { url, body, headers }
+
+            fireRequest(payload)
 
         }
         return next(action)
